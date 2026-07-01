0d25832fcbead23674c962587bdb08e2d85b4fc3
khounkham503-ui <khounkham503@github.com>
2026-07-01 13:35:44 +0700

    Optimize SEO for Meditation-Khoun and update base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Meditation-by-khoun/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Meditation-Khoun/' : '/',
   server: {
     port: 3000,
     open: true
